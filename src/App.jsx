206545acf1c64e98afd392b2a3f0f1c6053ce313
jarpanicolas <jarpanicolas@gmail.com>
2025-04-28 20:30:42 +0000

    Eliminado app ejemplo, primeros cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav>
+        <div className="home-button">
+            <a href="index.html">
+                    <img src="images/home.svg" alt="Home" className="home-icon"></img>
+                </a>
+            </div>
+        </nav>
+        <h1>Bienvenido a [Nombre]!</h1>
+    <main>
+
+    </main>
+    <div className="ubicacion-container">
+        <h2>¿Es esta su ubicación?</h2>
+        <div className="ubicacion-buttons">
+            <a href="page2.html" className="ubicacion-button yes">Sí</a>
+            <a href="page3.html" className="ubicacion-button no">No</a>
+        </div>
+    </div>
+    <script src="js/script.js"></script>
+
     </>
   )
 }
