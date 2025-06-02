8031edb16019fd88e3e4a10a8277f2265d154c44
parallals <frjelvez2021@udec.cl>
2025-06-02 03:23:24 -0400

    Readesigned login page to global design

--- src/components/LoginSignup.jsx
+++ src/components/LoginSignup.jsx
@@ -1,34 +1,50 @@
-import React from 'react' 
-import user_icon from '../assets/user.png';
-import email_icon from '../assets/email.png';
-import password_icon from '../assets/password.png';
+import { useEffect, useState } from "react";
+import email_icon from '../assets/mail.svg';
+import password_icon from '../assets/lock.svg';
+import open_eye from '../assets/eye.svg';
+import closed_eye from '../assets/eye-closed.svg';
 import {useNavigate} from 'react-router-dom';
 
 function LoginSignup() {
     let navigate = useNavigate();
 
+    useEffect(() => {
+    document.body.classList.add("login_bg");
+    return () => document.body.classList.remove("login_bg");
+  }, []);
+
+    const [showPassword, setShowPassword] = useState(false);
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
     return(
         <div className="login_container">
             <div className="login_header">
-                <div className="login_text">El login (no funciona)</div>
+                <div className="login_text">Please Log in</div>
             </div>
             <div className="login_inputs">
-                <div className="login_input">
-                    <img src={user_icon} alt="Usuario"/>
-                    <input type="user" placehoder="Usuario"/>
-                </div>
+                <h3 className="login_email_text">Correo</h3>
                 <div className="login_input">
                     <img src={email_icon} alt="Email"/>
-                    <input type="email" placeholder="Correo Electrónico"/>
+                    <input type="email"/>
                 </div>
+                <h3 className="login_pswrd_text">Contraseña</h3>
                 <div className="login_input">
                     <img src={password_icon} alt="Contraseña"/>
-                    <input type="password" placeholder="Contraseña"/>
+                    <input type={showPassword ? "text" : "password"} />
+                    <button className="login-pswrd-button" onClick={togglePasswordVisibility}>
+                         <img src={showPassword ? open_eye : closed_eye} alt="Toggle Password Visibility"/>
+                    </button>
                 </div>
             </div>
             <div className="submit-container">
                 <button className="login_submit_button" onClick={() => {navigate("/preguntas");}}>Login</button>
             </div>
+                
+            <p className="login-text">
+                ¿No tienes una cuenta? <a href="#">Registrate</a>
+            </p>
         </div>
         )
 }
