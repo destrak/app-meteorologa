7b62efff36b8e108eddf242c016eb23f52d66e79
parallals <frjelvez2021@udec.cl>
2025-06-04 14:50:55 -0400

    añadidas preguntas despues del login

---
+++ src/components/preguntas.js
@@ -0,0 +1,31 @@
+const preguntas = [
+    {
+        titulo: "Pregunta 1?",
+        opciones: [
+            {textoRespuesta: "Respuesta 1", isSelected: 1},
+            {textoRespuesta: "Respuesta 2", isSelected: 2},
+            {textoRespuesta: "Respuesta 3", isSelected: 3},
+            {textoRespuesta: "Respuesta 4", isSelected: 4}
+        ],
+    },
+    {
+        titulo: "¿Pregunta 2?",
+        opciones: [
+            {textoRespuesta: "Respuesta 1", isSelected: 2},
+            {textoRespuesta: "Respuesta 2", isSelected: 1},
+            {textoRespuesta: "Respuesta 3", isSelected: 3},
+            {textoRespuesta: "Respuesta 4", isSelected: 4}
+        ],
+    },
+    {
+        titulo: "¿Pregunta 3?",
+        opciones: [
+            {textoRespuesta: "Respuesta 1", isSelected: 2},
+            {textoRespuesta: "Respuesta 2", isSelected: 3},
+            {textoRespuesta: "Respuesta 3", isSelected: 1},
+            {textoRespuesta: "Respuesta 4", isSelected: 4}
+        ],
+    }
+];
+
+export default preguntas;
