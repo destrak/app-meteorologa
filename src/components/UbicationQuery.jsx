c6c7160ac4f3bff8608d2d047e58fedfcb1738ea
parallals <frjelvez2021@udec.cl>
2025-06-05 06:32:31 -0400

    diseño global home

--- src/components/UbicationQuery.jsx
+++ src/components/UbicationQuery.jsx
@@ -1,27 +1,13 @@
-import React, { useState } from 'react';
-import YesPopup from './YesPopup.jsx';
+
+import ZonaHoraria from './ZonaHoraria.jsx';
 function UbicationQuery(props) {
-    const [showYesPopup, setShowYesPopup] = useState(false);
 
-    const handleYesClick = () => {
-        setShowYesPopup(true); // Para la opción de "yes", muestra el clima
-    };
-
-    const handleClosePopup = () => {
-        setShowYesPopup(false); // Cerrar Yespopup
-    };
 
     return (
         <>
             <nav className="ubication-query-container">
                 <div className="ubicacion-content-wrapper">
-                    <div className="header-container">
-                        <h1 className="header-text">Bienvenido, {props.name}!</h1>
-                        <div className="ubicacion-buttons">
-                            <button className="ubicacion-button" onClick={handleYesClick}>Insertar Ubicación</button>
-                            {showYesPopup && <YesPopup onClose={handleClosePopup} />}
-                        </div>
-                    </div>
+
                 </div>
             </nav>
         </>
